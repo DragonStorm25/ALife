27f49abed1671d70b8835622bb1038894c5f560d
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-28 22:58:39 -0400

    Add particle constructors

--- src/main/ChargedParticle.java
+++ src/main/ChargedParticle.java
@@ -9,6 +9,19 @@ public class ChargedParticle extends Particle {
     public double maxRange = 1;
     public double minRange = 0.1;
 
+    public ChargedParticle(int mass, int charge) {
+        this(Vector2D.ZERO(), mass, charge);
+    }
+
+    public ChargedParticle(Vector2D pos, int mass, int charge) {
+        this(pos, Vector2D.ZERO(), mass, charge);
+    }
+
+    public ChargedParticle(Vector2D pos, Vector2D vel, int mass, int charge) {
+        super(pos, vel, mass);
+        this.charge = charge;
+    }
+
     public static Vector2D getChargeForce(ChargedParticle c1, ChargedParticle c2) {
         final Vector2D vectorBetween = c1.getPosition().plus(c2.getPosition().scale(-1));
         final double distance = vectorBetween.getMagnitude();
--- src/main/Particle.java
+++ src/main/Particle.java
@@ -6,6 +6,20 @@ public class Particle {
     private Vector2D pos, vel;
     private int mass;
 
+    public Particle(int mass) {
+        this(Vector2D.ZERO(), mass);
+    }
+
+    public Particle(Vector2D pos, int mass) {
+        this(pos, Vector2D.ZERO(), mass);
+    }
+
+    public Particle(Vector2D pos, Vector2D vel, int mass) {
+        this.pos = pos;
+        this.vel = vel;
+        this.mass = mass;
+    }
+
     public Vector2D getPosition() {
         return this.pos.clone();
     }
