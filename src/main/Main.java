f5c5e03fe9e484dd1f313e854d828e7627c032cc
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-29 00:17:27 -0400

    Basic quadtree test with 100 insertions

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package src.main;
 
+import src.particles.Particle;
 import src.util.QuadTree;
 import src.util.Vector2D;
 
@@ -7,6 +8,12 @@ public class Main {
     
     public static void main(String[] args) {
         QuadTree qt = new QuadTree(Vector2D.ZERO(), new Vector2D(128, 128));
+        System.out.println(qt);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++){
+                qt.insert(new Particle(new Vector2D(10*i, 10*j), 0));
+            }
+        }
         System.out.println(qt);
     }
 }
