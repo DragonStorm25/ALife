666fdb150fd735584a196b1fea6271e503eb7db3
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-28 22:34:38 -0400

    Add getChargeForce and charge constant

--- src/physics/ICharge.java
+++ src/physics/ICharge.java
@@ -1,7 +1,13 @@
 package src.physics;
 
 public interface ICharge {
+    public static final double CHARGE_CONSTANT = 1;
+
     public int charge = 0;
     public double maxRange = 1;
     public double minRange = 0.1;
+
+    public static double getChargeForce(ICharge c1, ICharge c2, double distance) {
+        return (CHARGE_CONSTANT * c1.charge * c2.charge) / (distance * distance);
+    }
 }
