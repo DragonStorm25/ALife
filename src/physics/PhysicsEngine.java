f971f1aa5a3ec962f1eb2058ed059c921369a0e4
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-28 23:18:03 -0400

    Create PhysicsEngine.java

---
+++ src/physics/PhysicsEngine.java
@@ -0,0 +1,15 @@
+package src.physics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import src.main.*;
+
+public class PhysicsEngine {
+    private List<Particle> particles;
+    private double deltaTime;
+
+    public PhysicsEngine() {
+        this.particles = new ArrayList<>();
+    }
+}
