45cc34c629a07a5ace67a501cefe081f247c03c6
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-29 17:31:38 -0400

    Make max and min range static, add range check for forces

--- src/particles/ChargedParticle.java
+++ src/particles/ChargedParticle.java
@@ -4,10 +4,10 @@ import src.util.Vector2D;
 
 public class ChargedParticle extends Particle {
     public static final double CHARGE_CONSTANT = 1;
+    public static final double MAX_RANGE = 1;
+    public static final double MIN_RANGE = 0.1;
 
     public int charge = 0;
-    public double maxRange = 1;
-    public double minRange = 0.1;
 
     public ChargedParticle(int mass, int charge) {
         this(Vector2D.ZERO(), mass, charge);
@@ -25,11 +25,13 @@ public class ChargedParticle extends Particle {
     public static Vector2D getChargeForce(ChargedParticle c1, ChargedParticle c2) {
         final Vector2D vectorBetween = c1.getPosition().plus(c2.getPosition().scale(-1));
         final double distance = vectorBetween.getMagnitude();
+        if (distance > MAX_RANGE || distance < MIN_RANGE) 
+            return Vector2D.ZERO();
         final double mag = (CHARGE_CONSTANT * c1.charge * c2.charge) / (distance * distance);
         return vectorBetween.normalized().scale(mag);
     }
 
     public String toString() {
-        return super.toString() + ", Charge: " + this.charge + " (Range: [" + minRange + "," + maxRange + "])";
+        return super.toString() + ", Charge: " + this.charge + " (Range: [" + MIN_RANGE + "," + MAX_RANGE + "])";
     }
 }
