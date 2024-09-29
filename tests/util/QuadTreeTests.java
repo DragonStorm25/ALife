2aee8582d64259c38f6f825bd644e719625a00ef
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-29 01:09:29 -0400

    Add quadtree tests

---
+++ tests/util/QuadTreeTests.java
@@ -0,0 +1,51 @@
+package tests.util;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+import src.particles.Particle;
+import src.util.QuadTree;
+import src.util.Vector2D;
+
+public class QuadTreeTests {
+    
+    @Test
+    void particleInsertion() {
+        QuadTree qt = new QuadTree(Vector2D.ZERO(), new Vector2D(128, 128));
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++){
+                qt.insert(new Particle(new Vector2D(10*i, 10*j), 0));
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++){
+                assertTrue(qt.particleAtPoint(new Vector2D(10*i, 10*j)));
+            }
+        }
+    }
+
+    @Test
+    void particlesWithinDistance() {
+        QuadTree qt = new QuadTree(Vector2D.ZERO(), new Vector2D(128, 128));
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++){
+                qt.insert(new Particle(new Vector2D(10*i, 10*j), 0));
+            }
+        }
+        List<Particle> nearby = qt.getParticlesWithinDistance(new Vector2D(10, 10), 15);
+        List<Vector2D> nearbyList = nearby.stream().map(Particle::getPosition).toList();
+        List<Vector2D> expectedList = new ArrayList<>();
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                expectedList.add(new Vector2D(i*10, j*10));
+        assertTrue(nearbyList.size() == expectedList.size() && nearbyList.containsAll(expectedList) && expectedList.containsAll(nearbyList));
+    }
+}
