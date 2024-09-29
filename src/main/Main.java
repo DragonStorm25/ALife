275aa80247e4c0a63a2ae7c77c7359e4beb0abec
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-29 01:10:01 -0400

    Remove some imports

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,24 +1,8 @@
 package src.main;
 
-import java.util.List;
-
-import src.particles.Particle;
-import src.util.QuadTree;
-import src.util.Vector2D;
-
 public class Main {
     
     public static void main(String[] args) {
-        QuadTree qt = new QuadTree(Vector2D.ZERO(), new Vector2D(128, 128));
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++){
-                qt.insert(new Particle(new Vector2D(10*i, 10*j), 0));
-            }
-        }
-        System.out.println(qt);
-        List<Particle> nearby = qt.getParticlesWithinDistance(new Vector2D(10, 10), 15);
-        for (Particle p : nearby) {
-            System.out.println(p);
-        }
+
     }
 }
--- tests/util/QuadTreeTests.java
+++ tests/util/QuadTreeTests.java
@@ -1,13 +1,9 @@
 package tests.util;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
