fcff61f7785b589dff4b5842239ba24daa5d0f4b
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-29 19:57:40 -0400

    Add simple render loop

--- src/graphics/Renderer.java
+++ src/graphics/Renderer.java
@@ -2,6 +2,8 @@ package src.graphics;
 
 import java.awt.*; 
 import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import src.particles.Particle;
 import src.physics.PhysicsEngine;
@@ -10,6 +12,9 @@ public class Renderer {
     private PhysicsEngine physicsEngine;
     private JFrame frame;
     private JPanel panel;
+    private final int FPS = 60;
+    private TimerTask renderTask = null;
+    private Timer renderTimer = null;
 
     public Renderer(PhysicsEngine pe) {
         this.frame = new JFrame("ALife");
@@ -29,12 +34,29 @@ public class Renderer {
 
     public void toggleShow() {
         this.frame.setVisible(!this.frame.isVisible());
+        this.startRendering();
     }
 
     public void render(Graphics g) {
         g.setColor(Color.red);
         for (Particle p : physicsEngine.idToParticle.values()) {
-            g.drawOval((int)p.getPosition().getX(), (int)p.getPosition().getY(), p.getMass(), p.getMass());
+            g.fillOval((int)p.getPosition().getX(), (int)p.getPosition().getY(), p.getMass(), p.getMass());
         }
+    }
+
+    public void startRendering() {
+        if (renderTimer != null)
+            renderTimer.cancel();
+
+        renderTask = new TimerTask() {
+
+            @Override
+            public void run() {
+                panel.revalidate();
+            }
+        };
+
+        renderTimer = new Timer("RenderTimer");//create a new Timer
+        renderTimer.scheduleAtFixedRate(renderTask, 0, 1000/FPS);
     }
 }
