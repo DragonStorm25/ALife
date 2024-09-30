c56af77e820ba9212042109345895e1f13800200
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-09-29 20:03:05 -0400

    First test (it works!)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,18 @@
 package src.main;
 
+import src.graphics.Renderer;
+import src.particles.ChargedParticle;
+import src.physics.PhysicsEngine;
+import src.util.Vector2D;
+
 public class Main {
     
     public static void main(String[] args) {
-
+        PhysicsEngine pe = new PhysicsEngine(0.1);
+        Renderer r = new Renderer(pe);
+        r.toggleShow();
+        pe.particles.insert(new ChargedParticle(new Vector2D(100, 100), 2, 10));
+        pe.particles.insert(new ChargedParticle(new Vector2D(101, 101), 2, 10));
+        pe.startEngine();
     }
 }
