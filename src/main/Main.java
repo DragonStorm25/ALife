7fed75c86e934738316490d075a5c117d6a972ae
DragonStorm25 <45071404+DragonStorm25@users.noreply.github.com>
2024-10-01 00:21:56 -0400

    Current test

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,17 +2,32 @@ package src.main;
 
 import src.graphics.Renderer;
 import src.particles.ChargedParticle;
+import src.particles.Particle;
 import src.physics.PhysicsEngine;
+import src.util.Profiler;
 import src.util.Vector2D;
 
 public class Main {
     
     public static void main(String[] args) {
+        // Profiler.SINGLETON.print = true;
+        // Profiler.SINGLETON.startProfiling();
+
         PhysicsEngine pe = new PhysicsEngine(0.1);
         Renderer r = new Renderer(pe);
-        r.toggleShow();
-        pe.particles.insert(new ChargedParticle(new Vector2D(100, 100), 2, 10));
-        pe.particles.insert(new ChargedParticle(new Vector2D(101, 101), 2, 10));
+        for (int i = 0; i < 10000; i++) {
+            pe.insert(new ChargedParticle(new Vector2D(512+(int)((Math.random()-0.5)*512), 512+(int)((Math.random()-0.5)*512)), 2, 2));
+        }
+        // Particle p1 = new ChargedParticle(new Vector2D(200, 100), 10, 20);
+        // Particle p2 = new ChargedParticle(new Vector2D(100, 100), 10, 20);
+        // Particle p3 = new Particle(new Vector2D(800, 100), 10);
+        // p1.setVelocity(new Vector2D(10, 0));
+        // pe.insert(p2);
+        // pe.insert(p1);
+        //pe.insert(p3);
+        // pe.insert(new ChargedParticle(new Vector2D(511, 100), 10, 20));
+        // pe.insert(new ChargedParticle(new Vector2D(513, 100), 10, 20));
         pe.startEngine();
+        r.toggleShow();
     }
 }
